c7433e126e9147cfd5c2f78f01938a4eb32729e2
DEV-PROG-specs <devendra.srivastava_cs19@gla.ac.in>
2020-08-30 22:04:21 -0700

    Add getters ,setters,toString,equal,hashcode method to Book.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,5 +1,5 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Devendra Srivastava(DEV-PROG-Spec)
  *  Date: 21/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
